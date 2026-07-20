c5921b4dfeb53715976a95f49b3d98072bf2a2c4
PinkThePanther <34952671+PinkThePanther@users.noreply.github.com>
2026-07-20 19:13:25 -0400

    added the user class db model

--- app.py
+++ app.py
@@ -15,8 +15,14 @@ CORS(app)
 
 
 class User(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
+with app.app_context():
+    db.create_all()
+   
 
 @app.get("/")
 def home():
