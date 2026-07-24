7b7595bd036dd9a578d573dbcf2fe9a0fd73f5d9
PinkThePanther <34952671+PinkThePanther@users.noreply.github.com>
2026-07-24 19:22:08 -0400

    Implement initial database persistence, verify User model round-trip, and begin backend architecture (services layer)

--- app.py
+++ app.py
@@ -19,10 +19,47 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime)
+
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    caption = db.Column(db.Text)
+    created_at = db.Column(db.DateTime)
+
+
+
+    class Comment(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+        user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+        content = db.Column(db.Text, nullable=False)
+        created_at = db.Column(db.DateTime)
+
+
 
 with app.app_context():
     db.create_all()
+
+
+    user = User (
+    username="testuser",
+    email="test@example.com",
+    password_hash="password"
+    )
+
+    #db.session.add(user)
+    #db.session.commit()
    
+
+    users = User.query.all()
+
+    for user in users:
+        print(user.username)
 
 @app.get("/")
 def home():
